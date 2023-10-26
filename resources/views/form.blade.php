649dfa1189239bdfc979be66534b5b79713e0eb8
eriktailor <erno22szabo@gmail.com>
2023-10-26 07:11:32 +0200

    Update form.blade.php

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -84,6 +84,10 @@
                 border-radius: 10px;
                 border: 3px dashed #dbdeea;
             }
+            .was-validated .form-control:valid {
+                border-color: #dee2e6 !important;
+                background-image: none;
+            }
         </style>
     </head>
     <body class="bg-light">
@@ -92,24 +96,18 @@
         <div class="container-md mt-5">
             <div class="col-xxl-5 col-xl-6 col-lg-8 mx-auto">
                 <h1 class="h1 fw-bold mb-5">Laravel 10 dropzone image upload with other form fields and validation</h1>
-                @if(session()->has('message'))
-                    <div class="alert alert-success mb-4">{{ session()->get('message') }}</div>
-                @endif
+                <div class="alert alert-success d-none mb-4" id="successMessage">The form was submitted successfully.</div>
                 <form class="dropzone overflow-visible p-0" id="formDropzone" method="POST" enctype="multipart/form-data" novalidate>
                     @csrf
                     <div class="form-group mb-3">
                         <label class="form-label text-muted opacity-75 fw-medium" for="formName">Name</label>
-                        <input class="form-control border-2 shadow-none fw-bold @error('name') is-invalid @enderror p-3" id="formName" name="name" value="{{ old('name') }}" type="text" required>
-                        @error('name')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
+                        <input class="form-control border-2 shadow-none fw-bold p-3" id="formName" name="name" type="text" required>
+                        <div class="invalid-feedback fw-bold">The name field is required.</div>
                     </div>
                     <div class="form-group mb-3">
                         <label class="form-label text-muted opacity-75 fw-medium" for="formEmail">Email</label>
-                        <input class="form-control border-2 shadow-none fw-bold @error('email') is-invalid @enderror p-3" id="formEmail" name="email" value="{{ old('email') }}" type="email" required>
-                        @error('email')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
+                        <input class="form-control border-2 shadow-none fw-bold p-3" id="formEmail" name="email" type="email" required>
+                        <div class="invalid-feedback fw-bold">The email field is required.</div>
                     </div>
                     <div class="form-group mb-4">
                         <label class="form-label text-muted opacity-75 fw-medium" for="formImage">Image</label>
@@ -128,8 +126,12 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="invalid-feedback fw-bold">Please upload an image.</div>
                     </div>
-                    <button class="btn btn-primary fw-medium py-3 px-4 mt-3" id="formSubmit" type="submit">Submit Form</button>
+                    <button class="btn btn-primary fw-medium py-3 px-4 mt-3" id="formSubmit" type="submit">
+                        <span class="spinner-border spinner-border-sm d-none me-2" aria-hidden="true"></span>
+                        Submit Form
+                    </button>
                 </form>
             </div>
         </div>
@@ -137,10 +139,13 @@
         <!-- Scripts -->
         <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.2/min/dropzone.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js"></script>
         <script>
             Dropzone.autoDiscover = false;
 
+            /**
+             * Setup dropzone
+             */
             $('#formDropzone').dropzone({
                 previewTemplate: $('#dzPreviewContainer').html(),
                 url: '/form-submit',
@@ -153,28 +158,52 @@
                 thumbnailWidth: 900,
                 thumbnailHeight: 600,
                 previewsContainer: "#previews",
-               
+                timeout: 0,
                 init: function() 
                 {
                     myDropzone = this;
 
-                    // submit button clicked
-                    $('#formSubmit').on('click', function(event) {
-                        event.preventDefault();
-                        event.stopPropagation();
-
-                        myDropzone.processQueue();
+                    // when file is dragged in
+                    this.on('addedfile', function(file) { 
+                        $('.dropzone-drag-area').removeClass('is-invalid').next('.invalid-feedback').hide();
                     });
                 },
                 success: function(file, response) 
                 {
-                    console.log('Successssss: '+response);
-                    //$('#form').submit();
-                },
-                error: function(file, response)
-                {
-                    console.log('Error: '+response);
-                    return false;
+                    // hide form and show success message
+                    $('#formDropzone').fadeOut(600);
+                    setTimeout(function() {
+                        $('#successMessage').removeClass('d-none');
+                    }, 600);
+                }
+            });
+
+            /**
+             * Form on submit
+             */
+            $('#formSubmit').on('click', function(event) {
+                event.preventDefault();
+                var $this = $(this);
+                
+                // show submit button spinner
+                $this.children('.spinner-border').removeClass('d-none');
+                
+                // validate form & submit if valid
+                if ($('#formDropzone')[0].checkValidity() === false) {
+                    event.stopPropagation();
+
+                    // show error messages & hide button spinner    
+                    $('#formDropzone').addClass('was-validated'); 
+                    $this.children('.spinner-border').addClass('d-none');
+
+                    // if dropzone is empty show error message
+                    if (!myDropzone.getQueuedFiles().length > 0) {                        
+                        $('.dropzone-drag-area').addClass('is-invalid').next('.invalid-feedback').show();
+                    }
+                } else {
+
+                    // if everything is ok, submit the form
+                    myDropzone.processQueue();
                 }
             });
 
